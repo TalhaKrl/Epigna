ec852f7f8d4bea2aec37ac2b21b88ad18688ba13
talhakrl <talhacemilkaral@gmail.com>
2025-08-15 10:26:51 -0500

    added more tests and reports

--- tests/ui/example.ui.test.js
+++ tests/ui/example.ui.test.js
@@ -4,3 +4,13 @@ test('homepage has title', async ({ page }) => {
   await page.goto('http://preprod.epigna.com');
   await page.locator('img[alt="Logo"]').isVisible();
 });
+
+test('can navigate to Apparel&Accessories and see Men category', async ({ page }) => {
+  await page.goto('http://preprod.epigna.com');
+  // Click the "Apparel&Accessories" tab
+  await page.getByRole('link', { name: /Apparel&Accessories/i }).click();
+  // Wait for 2 seconds so you can see the menu
+  await page.waitForTimeout(2000);
+  // Ensure the "Men" category link is visible under Clothing
+  await expect(page.locator('a[href="/browse/apparel-accessories/clothing/men"]')).toBeVisible();
+});
